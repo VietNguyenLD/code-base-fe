63a15990082e98d0487a02eb6836a4c4843508f1
bsv-vietnguyen <vietnguyen@bravesoft.com.vn>
2025-11-24 15:13:07 +0700

    feat: remove get feed

--- src/features/feed/api/getFeed.ts
+++
@@ -1,6 +0,0 @@
-import axiosClient from "@/shared/lib/axiosClient";
-import type { Post } from "@/entities/post/model/post";
-
-export function getFeedApi(): Promise<Post[]> {
-  return axiosClient.get("/posts/feed");
-}
